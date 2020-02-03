bd98d6a3956a20961b96a2723abc17b30a06cc74
Qi Zhang <qizhang@apple.com>
2020-02-02 16:13:34 -0800

    [TMGP4-85] Create Unit Test
    Fix wrong test configuration

--- userAuth/userAuth/src/test/java/com/apptime/auth/UserAuthApplicationTests.java
+++ userAuth/userAuth/src/test/java/com/apptime/auth/UserAuthApplicationTests.java
@@ -1,7 +1,8 @@
-package com.example.demo;
+package com.apptime.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 class UserAuthApplicationTests {
