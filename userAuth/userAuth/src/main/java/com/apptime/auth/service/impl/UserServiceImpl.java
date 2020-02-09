11cd38e14836eb5bb8f70459207f8ed52928ff18
Qi Zhang <qizhang@apple.com>
2020-02-09 11:22:50 -0800

    Revert "[TMGP4-85] Create Unit Test"
    This reverts commit d1014f5a74f4477e1cfa9c0ce4553667784cccfd.

--- userAuth/userAuth/src/main/java/com/apptime/auth/service/impl/UserServiceImpl.java
+++ userAuth/userAuth/src/main/java/com/apptime/auth/service/impl/UserServiceImpl.java
@@ -1,18 +1,11 @@
 package com.apptime.auth.service.impl;
 
-import com.apptime.auth.exception.UserExistingException;
-import com.apptime.auth.model.Roles;
 import com.apptime.auth.model.Users;
-import com.apptime.auth.repository.RoleRepository;
 import com.apptime.auth.repository.UserRepository;
 import com.apptime.auth.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -20,40 +13,7 @@ public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
 
     @Autowired
-    private RoleRepository roleRepository;
-
-    @Autowired
     private BCryptPasswordEncoder passwordEncoder;
-
-    @Override
-    public Users findByUsername(String username) {
-        return userRepository.findByUsername(username);
-    }
-
-    @Override
-    public void createUser(Users user) throws UserExistingException {
-        Users userInDb = userRepository.findByUsername(user.getUsername());
-        if (userInDb != null) {
-            throw new UserExistingException();
-        }
-        save(user);
-    }
-
-    @Override
-    public void save(Users user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        if (user.getRoles() == null || user.getRoles().isEmpty()) {
-            List<Roles> roleList = roleRepository.findAll();
-            for (Roles role : roleList) {
-                if ("user".equalsIgnoreCase(role.getRole())) {
-                    Set<Roles> set = Collections.singleton(role);
-                    user.setRoles(set);
-                    break;
-                }
-            }
-        }
-        userRepository.save(user);
-    }
 
     @Override
     public boolean resetPassword(String username, String oldPassword, String newPassword) {
---
+++ userAuth/userAuth/src/test/java/com/apptime/auth/test/AdminControllerTest.java
@@ -0,0 +1,91 @@
+package com.apptime.auth.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.apptime.auth.controller.AdminController;
+import com.apptime.auth.controller.UserController;
+import com.apptime.auth.model.Roles;
+import com.apptime.auth.model.Users;
+import com.apptime.auth.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+ 
+@ExtendWith(MockitoExtension.class)
+public class AdminControllerTest 
+{
+    @InjectMocks
+    UserController employeeController;
+     
+    @Mock
+    UserRepository userRepo;
+    MockMvc mockMvc;
+    @Mock
+    Users user;
+    @Mock
+    Roles role;
+    @Mock
+    BCryptPasswordEncoder pEncoder;
+     
+    @Test
+    public void testsignup() throws Exception 
+    {
+    	//Mockito.when(player.getWeapon()).thenReturn("Sword");
+    	Mockito.when(user.getPassword()).thenReturn("password");
+    	role.setRole("USER");
+    	role.setRole_id(3);
+    	 Set<Roles> set = new HashSet<Roles>(); 
+    	 set.add(role);
+
+    	Mockito.when(user.getRoles()).thenReturn(set);
+
+    	mockMvc.perform(MockMvcRequestBuilders.post("/signup")
+    			  .content(asJsonString(new Users()))
+    			  .contentType(MediaType.APPLICATION_JSON)
+    			  .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());  	
+    }
+    
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    
+    @Test
+    public void testSignin() throws Exception 
+    {
+    	mockMvc.perform(MockMvcRequestBuilders.get("/dashboard")
+    	    	  .content("")
+    	    	  .contentType(MediaType.APPLICATION_JSON).header("Authorization", "Basic test:test")
+    	    	  .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+    }
+
+}
