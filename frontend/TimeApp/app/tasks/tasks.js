120e4e7734d9bb51e130d1d639a7d073bd45afa8
yanisse <yanisse.rodriguez@upr.edu>
2020-02-11 15:15:18 -0600

    Update tasks.js

--- frontend/TimeApp/app/tasks/tasks.js
+++ frontend/TimeApp/app/tasks/tasks.js
@@ -2,12 +2,46 @@
 
 var app = angular.module('myApp.tasks', ['ui.router']);
 
-app.controller('tasksCtrl', function($scope, $state, $log, $mdDialog) {
-  	this.myDate = new Date();
+app.controller('tasksCtrl', function($scope, $state, $log, $mdDialog, timeFactory) {
+  	$scope.tsk = {
+  		eTime: "02:00 PM",
+  		eDate: new Date(),
+  		sTime: "01:00 PM",
+  		sDate: new Date()
+  	}
+
+  	$scope.categories = ['Test', 'TEST2'];
+
+  	$scope.times = timeFactory.getTimes();
+
+  	$scope.addCatg = true;
+
+  	$scope.myDate = new Date();
 
     this.onDateChanged = function() {
       $log.log('Updated Date: ', this.myDate);
     };
+
+     $scope.addCategory = function(d) {
+     	$scope.addCatg = d;
+ 	};
+
+ 	 $scope.showConfirm = function(ev, i) {
+    // Appending dialog to document.body to cover sidenav in docs app
+    var confirm = $mdDialog.confirm()
+          .title('Are you sure you want to delete the task?')
+          .textContent('The selected task ' + i + ' will be deleted from the system.')
+          .ariaLabel('Delete Task')
+          .targetEvent(ev)
+          .ok('Confirm')
+          .cancel('Cancel');
+
+    $mdDialog.show(confirm).then(function() {
+      $scope.status = 'You decided delete the task.';
+    }, function() {
+      $scope.status = 'You decided to keep the task.';
+    });
+  };
 
     $scope.showAdvanced = function(ev) {
 	    $mdDialog.show({
@@ -25,6 +59,23 @@ app.controller('tasksCtrl', function($scope, $state, $log, $mdDialog) {
 	    });
 	};
 
+	$scope.showEdit = function(ev) {
+	    $mdDialog.show({
+	      controller: DialogController,
+	      templateUrl: 'tasks/dialog2.tmpl.html',
+	      parent: angular.element(document.body),
+	      targetEvent: ev,
+	      clickOutsideToClose:true,
+	      fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+	    })
+	    .then(function(answer) {
+	      $scope.status = 'You said the information was "' + answer + '".';
+	    }, function() {
+	      $scope.status = 'You cancelled the dialog.';
+	    });
+	};
+
+
 	function DialogController($scope, $mdDialog) {
     $scope.hide = function() {
       $mdDialog.hide();
@@ -37,7 +88,28 @@ app.controller('tasksCtrl', function($scope, $state, $log, $mdDialog) {
     $scope.answer = function(answer) {
       $mdDialog.hide(answer);
     };
-	}	
+	}
+
+	// $scope.showPrompt = function(ev) {
+ //    // Appending dialog to document.body to cover sidenav in docs app
+ //    var confirm = $mdDialog.prompt()
+ //      .title('What would you name your dog?')
+ //      .textContent('Bowser is a common name.')
+ //      .placeholder('Dog name')
+ //      .ariaLabel('Dog name')
+ //      .initialValue('Buddy')
+ //      .targetEvent(ev)
+ //      .required(true)
+ //      .ok('Okay!')
+ //      .cancel('I\'m a cat person');
+
+ //    $mdDialog.show(confirm).then(function(result) {
+ //      $scope.status = 'You decided to name your dog ' + result + '.';
+ //    }, function() {
+ //      $scope.status = 'You didn\'t name your dog.';
+ //    });
+ //  };
+
 });
 
 // app.config(['$routeProvider', function($routeProvider) {
@@ -47,6 +119,3 @@ app.controller('tasksCtrl', function($scope, $state, $log, $mdDialog) {
 //   });
 
 // }]);
-
-
-
